5bf6bc5e2ccb723c2387d2e1f9fa58285a8b26b2
Pedro Rafael <pedro.rafael.marinho@gmail.com>
2021-04-14 22:04:51 -0300

    Update package. Various functions and documentation have been added.

---
+++ R/exp-G.R
@@ -0,0 +1,96 @@
+#' Generates the density distribution or the accumulated distribution function Exp-G
+#'
+#' @param G Cumulative distribution function (baseline)
+#' @param density By default, `density = FALSE`, that is, it returns the cumulative
+#' distribution function \eqn{Exp-G}. If `density = TRUE`, then the \eqn{Exp-G} density
+#' function will be reinforced.
+#'
+#' @return The probability density function or the cumulative distribution
+#' function will be returned.
+#' @export
+#' @details  This function generates the probability density function or the
+#' accumulated distribution function \eqn{Exp-G}, for any specified \eqn{G}.
+#' Exp-G will have one more parameter, being it \eqn{s > 0}.
+#' @examples
+#' pdf_w <- function(x, alpha, beta) {
+#'    pweibull(q = x, shape = alpha, scale = beta)
+#' }
+#'
+#' # Exp-Weibull density:
+#' pdf_exp_w <- exp_G(G = pdf_w, density = TRUE)
+#'
+#' # Testing if exp_w is density:
+#' integrate(
+#'    f = pdf_exp_w,
+#'    lower = 0,
+#'    upper = Inf,
+#'    alpha = 1.2,
+#'    beta = 1.3,
+#'    s = 2.1
+#' )
+#'
+#' # Exp-Weibull (accumulated distribution):
+#' cdf_exp_w <- exp_G(G = pdf_w, density = FALSE)
+#'
+#' # Testing whether it is cumulative distribution:
+#' cdf_exp_w(x = 0, alpha = 1.2, beta = 1.3, s = 2.1) # in 0
+#' cdf_exp_w(x = Inf, alpha = 1.2, beta = 1.3, s = 2.1) # in Inf
+exp_G <- function(G, density = FALSE) {
+
+  if(density){
+    function(x, s, ...) {
+      numDeriv::grad(func = function(x) G(x, ...) ^ s, x = x)
+    }
+  } else {
+    function(x, s, ...) {
+      G(x = x, ...) ^ s
+    }
+  }
+}
+
+
+#' Mathematical term used in the sum of equation 19 of the paper
+#'
+#' @param n Value that indexes a sum of equation 19.
+#' @param k Value that indexes a sum of equation 19.
+#' @param alpha Additional parameters introduced by the new model proposed in the paper.
+#' @param theta Additional parameters introduced by the new model proposed in the paper.
+#' @details \eqn{n} and \eqn{k} are indexed values in the sums of equation
+#' 19 of the paper.
+#' @return Return of expression  \eqn{\upsilon_{n,k}(\alpha, \beta)} ot the paper.
+#' @export
+upsilon <- function(n, k, alpha, theta, p_n) {
+  (-1)^k * choose(-n, k) * alpha^(-n) * (1 - alpha^(-1))^k * p_n(n = n, theta = theta)
+}
+
+#' @export
+eq_19 <- function(N = 100L, K = 100L, alpha, theta, p_n) {
+
+  inner_sum <- function(n) {
+    step_1 <- function(k) {
+      upsilon(
+        n = n,
+        k = k,
+        alpha = alpha,
+        theta = theta,
+        p_n = p_n
+      )
+    }
+    vapply(
+      X = 0L:K,
+      FUN = step_1,
+      FUN.VALUE = double(length = 1L)
+    ) %>% sum()
+  }
+
+  upsilon <-
+    vapply(
+    X = 1L:N,
+    FUN = inner_sum,
+    FUN.VALUE = double(length = 1L)
+  ) %>% sum()
+
+
+
+}
+
---
+++ R/power_series_distributions.R
@@ -0,0 +1,53 @@
+#' Zero-truncated Poisson probability function
+#'
+#' @param n Function support, with \eqn{n = 1, 2, \cdots}.
+#' @param theta Theta parameter, such that \eqn{\theta > 0}.
+#'
+#' @return Calculated probability (vector)
+#' @export
+#'
+#' @examples
+#' sapply(
+#'    X = 1L:1000L,
+#'    FUN = function(i)
+#'    pf_ztp(n = i, theta = 1.2)
+#' ) %>% sum
+pf_ztp <- function(n, theta) {
+  theta ^ n / ((exp(theta) - 1) * factorial(n))
+}
+
+#' Logarithmic probability function
+#'
+#' @param n Function support, with \eqn{n = 1, 2, \cdots}.
+#' @param theta A probability value, i.e, \eqn{0 < \theta < 1}.
+#'
+#' @return Calculated probability (vector)
+#' @export
+#'
+#' @examples
+#' sapply(
+#'      X = 1L:1000L,
+#'      FUN = function(i)
+#'      pf_logarithmic(n = i, theta = 0.5)
+#' ) %>% sum
+pf_logarithmic <- function(n, theta) {
+  actuar::dlogarithmic(x = n, prob = theta, log = FALSE)
+}
+
+#' Geometric probability function
+#'
+#' @param n Function support, with \eqn{n = 1, 2, \cdots}.
+#' @param theta A probability value, i.e, \eqn{0 < \theta < 1}.
+#'
+#' @return Calculated probability (vector)
+#' @export
+#'
+#' @examples
+#' sapply(
+#'    X = 1L:1000L,
+#'    FUN = function(i)
+#'    pf_geometric(n = i, theta = 0.5)
+#' ) %>% sum
+pf_geometric <- function(n, theta) {
+  dgeom(x = n, prob = theta)
+}
